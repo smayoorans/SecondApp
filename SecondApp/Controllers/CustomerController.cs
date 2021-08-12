1dc6c663f28120b5f2329986ec05f6907cfa0847
Mayooran <mayooran@unicomtechnology.co.uk>
2021-08-12 18:15:33 +0530

    Initial commit

---
+++ SecondApp/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
